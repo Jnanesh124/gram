6c717c223846ae143c5352cb663f391aebbe4a70
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-03-01 23:14:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 29942004
 API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
 BOT_TOKEN    = "6765313019:AAHKonji9_lBdTgJSMsVDbEUReloafojVtA"
-SESSION      = "BQHI4PQAHoFbES_PAtjttRsH1uxWTihntOKRAIW6-mX2W_gaMGp-zJr0rezxFJeiLq_NmWNXxQxMnPr550p8J0YiyXwNRTFsiCQsFadlolp40vRmemS4mBoxKFcEX0xbxArZcg-Dyu8WJIxTGpOZk9LFppl6sLOacwBltHsBZ63-Evwujdpb6-QlMGCrVHetpWyUKPoAK0mVdQiPpiBpmYxL26LexiA-PM_Qd3sv1wHReMneNrhaNABFViXYiYoeZGplx3d_B6lxLEcm-q7FnikOxNZD4VDTlrk2DpTmU9i6hSMi-AyO2a-8DZgCSX1dYhFVaLOIlG4BzM6efWbu5Y-K4jOfvAAAAABkI-HqAA"
+SESSION      = "BQHI4PQAWwqXQI_0ySb39EgQKYgbtKAQ7FFI_3XBqVCvJb06k6C90OXSYICoB_8usVS7LNdw4aED_syCkEr-fbiH3HnNbJIiAx_5wrOH_lX1FuAMeeV4agfdPn0VXfTRwghULtd6bTxm9TpqmJkw0BGsAw7OG3LgB2Tn1bim2UOPrZgzuzRcMGkBa0ibomTR-OjhYDU098x_4dSOy-MOOUH35Xk0f1ZuMtJHHkjJwn_ZkviBb2g6k4IaOMrgIQrPGCgFpYv6io6AcbYCbWEiqyrjNN0mFtUGnmG1fPuOmkscwvE5Mta_oLIcWSkbhuVzfTLhNKTD966tNRC4b79kMxPKgwW1GwAAAABkI-HqAA"
 DATABASE_URI = "mongodb+srv://ottmovies:ottmovies@cluster0.bejrjkt.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
 ADMIN        = 6605647659
