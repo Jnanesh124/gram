570b7fad9bbfa6326d179214447101391c64c186
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-03-03 21:19:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 20846870
 API_HASH     = "a864594cb1752ff03948066e529fcf0a"
-BOT_TOKEN    = "6a6df8006df3cb56edce33056d37baca"
+BOT_TOKEN    = "6765313019:AAGYzdlzJ-w5TNM9npSbBg4sGyX-44K5uHA"
 SESSION      = "BAEwOXIAfdIPlVRsMHxjDlplJzswkdzLqD48qhUpxbmIjLx5IFzqfnVBf4mBAEpmjrEfvuH2ceKJSHX2GYvBjzucyXBpqOEW6KrezUKwjmH0Eezv7Q_BLMIy5Cn5uOWCzYw3VLFfDB6p35-bghzs5a41XFny9GHfOTOZRFmWZsGqeaK51gD-2tcTbuj1b_CHMVHkdL_908c9ORo46VHUcLE9EbkCFBmsLB3flub4oQ_mvOgDGRFsedaWKDifeqT40NBKk3wumjDb6R4Z-hF_TxSIn-W6gd6-QuHQ7ge-fHVFqF2o5G0czQOhAl7oqWpAOKy6QpGe2tcFGvP-ONTls2XVWmMcAwAAAAGnLbV2AA"
 DATABASE_URI = "mongodb+srv://ottmovies:ottmovies@cluster0.bejrjkt.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001997863812
